9d7c989d3871833e5c0973df18dcae67fc541396
eicon <3497998763@qq.com>
2025-10-24 17:40:45 +0800

    函数

--- s-aes.py
+++ s-aes.py
@@ -35,7 +35,7 @@ class S_AES:
     #初始化A-AES实例
     def __init__(self,key):
         if not (0 <= key < 0x10000):
-            raise ValueError("密钥必须是16位整数")
+            raise ValueError("密钥必须是16位二进制数")
         self.master_key = key
         self.round_keys = self.key_expansion(key)
 
@@ -83,36 +83,80 @@ class S_AES:
 
         #生成16位轮密钥
         round_key0 = (w0 << 8) | w1
-        round_key1 = (w2 <<8)
+        round_key1 = (w2 << 8) | w3
+        round_key2 = (w4 << 8) | w5
+
+        return [round_key0,round_key1,round_key2]
+
 
 
     #轮密钥加
     def add_round_key(self, state, round_key):
-        #判断明文是否满足16位
-        if len(state) != 16:
-            raise ValueError("明文必须是16bits")
-        
-        new_state = state^roundkey
+        round_key_state = self.int_to_state(round_key)
+        new_state = [[0,0],[0,0]]
+
+        for i in range(2):
+            for j in range(2):
+                new_state[i][j] = state[i][j] ^ round_key_state[i][j]
+                
         return new_state
+    
 
     #半字节替代*    
     def sub_nibbles(self, state, s_box):
+        new_state = [[0,0],[0,0]]
+
+        for i in range(2):
+            for j in range(2):
+                new_state[i][j] = self.sub_nibble(state[i][j],s_box)
+
+        return new_state
         
         
     #行移位
     def shift_rows(self, state):
+        #第一行不变，第二行交换
+        return[
+            [state[0][0],state[0][1]],
+            [state[1][1],state[1][0]]
+        ]
 
 
     #GF(2^4)乘法
     def gf_mult(self, a, b):
+        p = 0
+        for _ in range(4):
+            if b & 1:
+                p ^= a
+            hi_bit_set = a & 0x8
+            a <<= 1
+            a &= 0xF
+            if hi_bit_set:
+                a ^= 0x3
+            b >>= 1
+        return p
 
 
     #列混淆
     def mix_columns(self, state, matrix):
+        new_state = [[0,0],[0,0]]
+
+        for j in range(2):
+            s0 = state[0][j]
+            s1 = state[1][j]
+
+            new_state[0][j] = self.gf_mult(matrix[0][0],s0) ^ self.gf_mult(matrix[0][1], s1)
+            new_state[1][j] = self.gf_mult(matrix[1][0],s0) ^ self.gf_mult(matrix[1][1], s1)
+
+        return new_state
 
 
     #加密单个16位分组
     def encrypt_block(self, plaintext):
+        if not (0 <= plaintext <= 0xFFFF):
+            raise ValueError("明文必须是16位二进制数")
+        
+
 
 
     #解密单个16位分组
